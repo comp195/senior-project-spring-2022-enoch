b9c7fe3a605bab8b3df4bae5be71aa5cc980609e
avander8 <a_vanderpool@u.pacific.edu>
2022-05-06 23:00:28 -0700

    Updated Controller NRF comms
    updated NRF communication to support sending specific inputs from my new keyboard

--- Controller/Controller_NRFRadio/Controller_NRFRadio/Controller_NRFRadio.ino
+++ Controller/Controller_NRFRadio/Controller_NRFRadio/Controller_NRFRadio.ino
@@ -4,6 +4,7 @@
 NRFLite _radio;
 uint8_t _data = 0;
 String ControllerInput;
+boolean replay = true;
 
 void setup()
 {
@@ -19,49 +20,73 @@ void loop()
     {
     ControllerInput = Serial.readStringUntil('\n');
     ControllerInput.trim();
-      if (ControllerInput.equals("Forward")) 
+      if (ControllerInput.equals("t")) 
       {
         _data = 0;
         _radio.send(0, &_data, sizeof(_data));
       }
-      else if (ControllerInput.equals("Backward")) 
+      else if (ControllerInput.equals("a"))
       {
         _data = 1;
         _radio.send(0, &_data, sizeof(_data));
       }
-      else if (ControllerInput.equals("Left")) 
+      else if (ControllerInput. equals("s")) 
       {
         _data = 2;
         _radio.send(0, &_data, sizeof(_data));
       }
-      else if (ControllerInput.equals("Right")) 
+      else if (ControllerInput.equals("d")) 
       {
         _data = 3;
         _radio.send(0, &_data, sizeof(_data));
       }
-      else if (ControllerInput.equals("EMO")) 
+      else if (ControllerInput.equals("f")) 
       {
         _data = 4;
+        _radio.send(0, &_data, sizeof(_data));
+      }
+      else if (ControllerInput.equals("w")) 
+      {
+        _data = 5;
+        _radio.send(0, &_data, sizeof(_data));
+      }
+      else if (ControllerInput.equals("e")) 
+      {
+        _data = 6;
+        _radio.send(0, &_data, sizeof(_data));
+      }
+      else if (ControllerInput.equals("r")) 
+      {
+        _data = 7;
+        _radio.send(0, &_data, sizeof(_data));
+      }
+      else if (ControllerInput.equals("i")) 
+      {
+        _data = 8;
+        _radio.send(0, &_data, sizeof(_data));
+      }
+      else if (ControllerInput.equals("j")) 
+      {
+        _data = 9;
+        _radio.send(0, &_data, sizeof(_data));
+      }
+      else if (ControllerInput.equals("m")) 
+      {
+        _data = 10;
+        _radio.send(0, &_data, sizeof(_data));
+      }
+      else if (ControllerInput.equals("k")) 
+      {
+        _data = 11;
         _radio.send(0, &_data, sizeof(_data));
       }
       else 
       {
         Serial.println("bad ControllerInput");
       }
+      
 
     }
-    //Add 1 every time loop occurs (this is for testing purposes)
-    //_data++;
-    // Send data to the radio with Id = 0
-    //_radio.send(0, &_data, sizeof(_data));
-    
-    //Add 1 every time loop occurs (this is for testing purposes)
-    //_data++;
-    // Send data to the radio with Id = 0
-    _radio.send(0, &_data, sizeof(_data));
-    
-    //Toggle delay for testing purposes
-    //delay(1000);
 
     if (_radio.hasAckData())
     {
